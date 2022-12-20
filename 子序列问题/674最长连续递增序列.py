9a7216fd54efa175437fce3434199619b9878217
“wangruiguo” <wangrg@dchealth.com>
2022-12-20 16:08:13 +0800

    leetcode

--- 子序列问题/674最长连续递增序列.py
+++ 子序列问题/674最长连续递增序列.py
@@ -19,7 +19,14 @@ from typing import List
 class Solution:
 
     def findlengthoflist(self, nums: List[int]) -> int:
-        pass
+        if len(nums) <= 1:
+            return len(nums)
+        dp = [1]*len(nums)
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                dp[i] = dp[i-1]+1
+            
+        return max(dp)
 
 
 
